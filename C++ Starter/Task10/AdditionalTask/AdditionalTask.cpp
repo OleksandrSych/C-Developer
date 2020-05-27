c2f3e44b3114f0c4a7dc9cd1d0dff207d22aa483
Oleksandr Sych <sychalexandr@ukr.net>
2020-05-27 22:17:19 +0300

    Task10 completed

---
+++ C++ Starter/Task10/AdditionalTask/AdditionalTask.cpp
@@ -0,0 +1,92 @@
+﻿
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+void PrintArray(int[7][7]);
+
+int main()
+{
+
+	int myArray[7][7];
+	for (size_t i = 0; i < 7; i++)
+	{
+		for (size_t j = 0; j < 7; j++)
+		{
+			myArray[i][j] = rand() % 9 + 0;
+		}
+	}
+	PrintArray(myArray);
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	//Sort
+	for (size_t i = 0; i < 7; i++)
+	{
+		int min = myArray[i][0];
+		int max = myArray[i][0];
+		int minIndx = 0;
+		int maxIndx = 0;
+		for (size_t j = 0; j < 7; j++)
+		{
+			if (myArray[i][j] > max)
+			{
+				max = myArray[i][j];
+				maxIndx = j;
+			}
+			if (myArray[i][j] < min)
+			{
+				min = myArray[i][j];
+				minIndx = j;
+			}
+		}
+		if (maxIndx - minIndx > 2)
+		{
+
+			for (size_t k = 0; k < maxIndx - minIndx; k++)
+			{
+				for (size_t m = 0; m < maxIndx - minIndx; m++)
+				{
+					if (myArray[i][minIndx + 1] > myArray[i][minIndx + 1 + m])
+					{
+						int temp = myArray[i][minIndx + 1 + m];
+						myArray[i][minIndx + 1 + m] = myArray[i][minIndx + 1];
+						myArray[i][minIndx + 1] = temp;
+					}
+				}
+			}
+		}
+		if (minIndx -  maxIndx >  2)
+		{
+
+			for (size_t k = 0; k < minIndx - maxIndx; k++)
+			{
+				for (size_t m = 0; m < minIndx - maxIndx; m++)
+				{
+					if (myArray[i][maxIndx + 1] < myArray[i][maxIndx + 1 + m])
+					{
+						int temp = myArray[i][maxIndx + 1 + m];
+						myArray[i][maxIndx + 1 + m] = myArray[i][maxIndx + 1];
+						myArray[i][maxIndx + 1] = temp;
+					}
+				}
+			}
+		}
+	}
+
+	PrintArray(myArray);
+}
+
+
+void PrintArray(int myArray[7][7])
+{
+	for (size_t i = 0; i < 7; i++)
+	{
+		for (size_t j = 0; j < 7; j++)
+		{
+			cout << myArray[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+
---
+++ C++ Starter/Task10/Task2/Task2.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int size = 0;
+	int* myArray = new int[0];
+	int num;
+	do
+	{
+		cout << endl;
+		cin >> num;
+		if (num >= 0)
+		{
+			int* tempArray = new  int[size + 1];
+			for (size_t i = 0; i < size; i++)
+			{
+				tempArray[i] = myArray[i];
+			}
+			tempArray[size++] = num;
+			myArray = tempArray;
+
+			//print result			
+			for (size_t i = 0; i < size; i++)
+			{
+				cout << myArray[i];
+			}
+		}
+	} while (num >= 0);
+
+	//print result
+	cout << "End" << endl;
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << myArray[i];
+	}
+}
